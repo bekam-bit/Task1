ab35031c2ca35705e04b02d071b421690820d151
bekam-bit <bekamyoseph1@gmail.com>
2025-03-10 20:41:15 +0300

    Update UserCard.jsx

--- UserCard.jsx
+++ UserCard.jsx
@@ -1,22 +1,13 @@
-//  solved problem using state 
-import { useState } from 'react';
+// solved problem using Props
 import './style.css';
-//Declaring count as state
-function Counter() {
-    const [count, setCount] = useState(0);
-//It decreases until the value is 0
-    const decrement = () => {
-        if (count > 0) {
-            setCount(count - 1)
-        }
-    }
-    return (
-        <div>
-            <p className='count'>{count}</p>
-            <button className='btn-1' onClick={() => setCount(count + 1)}>Increment</button>
-            <button className='btn-2' onClick={decrement}>Decrement</button>
-            <button className='btn-3' onClick={() => setCount(0)}>Reset</button>
-        </div>
-    )
+//Declaring name and age as props
+function UserCard({ Name, Age, Email }) {
+  return (
+    <div className='usercard'>
+      <p>My name is {Name}</p>
+      <p>I am {Age} years old</p>
+      <p>Email:{Email}</p>
+    </div>
+  )
 }
-export default Counter;
+export default UserCard;
